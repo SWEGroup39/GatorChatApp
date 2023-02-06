cc715adc204c88b170f2c0ab156ec1d522202083
kevinc3n <92806132+kevinc3n@users.noreply.github.com>
2023-02-05 23:51:48 -0500

    Added query search example, cleared database with each run, and printed each message in the database with each run

--- GatorChat_Database.go
+++ GatorChat_Database.go
@@ -22,25 +22,54 @@ type Message struct {
 }
 
 func main() {
+
+	//CONNECTS TO THE SQLITE DATABASE KNOWN AS "test.db"
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 
 	if err != nil {
 		panic("Error: Failed to connect to database.")
 	}
 
+	//TABLE NAME IS THE PLURALIZED VERSION OF THE STRUCT NAME IN SNAKE CASE
+	//CLEARS THE ENTIRE DATABASE
+	err = db.Exec("DELETE FROM messages").Error
+	if err != nil {
+		panic(err)
+	}
+
+	//GENERATES A NEW TABLE IF ONE WAS NOT MADE OR MIGRATES ALL EXISTING DATA IF IT DOES EXIST
 	db.AutoMigrate(&Message{})
 
 	//GENERATE MESSAGE ENTRIES
-	db.Create(&Message{Text: "Test Message", SenderID: 12345, ReceiverID: 1359})
+	db.Create(&Message{Text: "Hello", SenderID: 12345, ReceiverID: 1359})
 
 	//QUERY FOR THE FIRST ENTRY IN THE TABLE
 	var textMessage Message
 	db.First(&textMessage, 1)
 
-	textMessage.Text = "Replacement Text"
+	//UPDATE THE TEXT MESSAGE OF THE TEXTMESSAGE VARIABLE
+	textMessage.Text = "Hi"
 	db.Save(&textMessage)
 
-	fmt.Println("db.First(&textMessage, 1) returns: ")
+	fmt.Println("The first message in the database.")
 	fmt.Println("Message: ", textMessage.Text)
 	fmt.Println("Message: ", textMessage.CreatedAt)
+
+	//CLEAR THE VARIABLE
+	textMessage = Message{}
+	//LOCATE THE FIRST MESSAGE IN THE DATABASE THAT HAS THE MATCHING TEXT
+	db.Where("Text = ?", "Hi").First(&textMessage)
+	//%v IS A FORMAT VERB THAT WILL BE REPLACED WITH THE RESPECTIVE CONTENT AFTER THE COMMA
+	fmt.Println("The first instance of 'Hi' in the database.")
+	fmt.Printf("%v (%v)", textMessage.Text, textMessage.CreatedAt)
+	fmt.Println()
+
+	//PRINT ALL MESSAGES IN DATABASE
+	var messages []Message
+	db.Find(&messages)
+	fmt.Println("All messages:")
+	for _, message := range messages {
+		fmt.Println("Message:", message.Text, "SenderID:", message.SenderID, "Time Stamp:", message.CreatedAt)
+	}
+
 }
