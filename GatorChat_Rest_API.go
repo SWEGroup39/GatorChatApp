831b39430396654a7dbbd533fa737094136cd457
kevinc3n <92806132+kevinc3n@users.noreply.github.com>
2023-02-13 20:38:46 -0500

    First attempt at integrating MySQL database with REST API

--- GatorChat_Rest_API.go
+++ GatorChat_Rest_API.go
@@ -1,92 +1,104 @@
 package main
 
-// this code is interfaced via Postman
-
 import (
 	"encoding/json"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-// Text Struct
-type Text struct {
-	ID      string  `json:"id:"`
-	Sender  *Sender `json:"sender"`
-	Message string  `json:"message"`
+// Connects to the MySQL database on a host and port, using the database name, username, and password
+var dsn = "swegroup39:8wWrp52ey^2^@tcp(gator-chat.mysql.database.azure.com:3306)/user_messages?parseTime=true&tls=true&charset=utf8mb4"
+var db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+type UserMessage struct {
+	gorm.Model
+	Message    string `json:"message"`
+	SenderID   string `json:"sender_id"`
+	ReceiverID string `json:"receiver_id"`
 }
 
-// Sender Struct
-type Sender struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
-
-// Init texts var as a slice Text struct
-var texts []Text
-
-// Get All Texts
-func getTexts(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(texts)
-}
-
-// Create a New Text
-func createText(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var text Text
-	_ = json.NewDecoder(r.Body).Decode(&text)
-	text.ID = strconv.Itoa(rand.Intn(10000000))
-	texts = append(texts, text)
-	json.NewEncoder(w).Encode(text)
-}
-
-// Edit a Text
-func editText(w http.ResponseWriter, r *http.Request) {
+// Get a specific message by ID
+func getMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range texts {
-		if item.ID == params["id"] {
-			texts = append(texts[:index], texts[index+1:]...)
-			var text Text
-			_ = json.NewDecoder(r.Body).Decode(&text)
-			text.ID = params["id"]
-			texts = append(texts, text)
-			json.NewEncoder(w).Encode(text)
-			return
-		}
+	var message UserMessage
+	result := db.First(&message, params["id"])
+	if result.Error != nil {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
 	}
+	json.NewEncoder(w).Encode(message)
+}
+
+// Create a new message
+func createMessage(w http.ResponseWriter, r *http.Request) {
+	var message UserMessage
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	result := db.Create(&message)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(message)
+}
+
+// Edit a message
+func editMessage(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var message UserMessage
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	result := db.Model(&UserMessage{}).Where("id = ?", params["id"]).Updates(&message)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	if result.RowsAffected == 0 {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(message)
 }
 
 // Delete a Message
-func deleteText(w http.ResponseWriter, r *http.Request) {
+func deleteMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range texts {
-		if item.ID == params["id"] {
-			texts = append(texts[:index], texts[index+1:]...)
-			break
-		}
+	var userMessage UserMessage
+	result := db.Where("id = ?", params["id"]).Delete(&userMessage)
+	if result.RowsAffected == 0 {
+		http.NotFound(w, r)
+		return
 	}
-	json.NewEncoder(w).Encode(texts)
+	json.NewEncoder(w).Encode("Message deleted successfully")
 }
 
 func main() {
 	// Init Router
 	r := mux.NewRouter()
 
-	// Mock Text Data for testing purposes
-	texts = append(texts, Text{ID: "1", Sender: &Sender{Firstname: "Johnny", Lastname: "Struckman"}, Message: "Hello"})
-	texts = append(texts, Text{ID: "2", Sender: &Sender{Firstname: "Strohnny", Lastname: "Juckman"}, Message: "Hi"})
+	// // Connect to MySQL database
+	// err = db.AutoMigrate(&UserMessage{})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	// Text Route Handlers / Endpoints
-	r.HandleFunc("/api/texts", getTexts).Methods("GET")
-	r.HandleFunc("/api/texts", createText).Methods("POST")
-	r.HandleFunc("/api/texts/{id}", editText).Methods("PUT")
-	r.HandleFunc("/api/texts/{id}", deleteText).Methods("DELETE")
+	r.HandleFunc("/api/messages/{id}", getMessage).Methods("GET")
+	r.HandleFunc("/api/messages", createMessage).Methods("POST")
+	r.HandleFunc("/api/messages/{id}", editMessage).Methods("PUT")
+	r.HandleFunc("/api/messages/{id}", deleteMessage).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
