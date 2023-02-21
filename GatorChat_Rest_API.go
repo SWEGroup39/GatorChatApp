d332320ee92f39f824f130603725eceaf2e10e8a
kevinc3n <92806132+kevinc3n@users.noreply.github.com>
2023-02-20 22:43:39 -0500

    Added validation check for POST, a delete table function, and a search by message function (PUT)

--- GatorChat_Rest_API.go
+++ GatorChat_Rest_API.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+	"net/url"
 
 	"github.com/gorilla/mux"
 	"gorm.io/driver/mysql"
@@ -69,6 +71,19 @@ func getAllMessages(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(messages)
 }
 
+func searchMessage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var messages []UserMessage
+	searchQuery, _ := url.QueryUnescape(params["search"])
+	result := db.Where("Message = ?", searchQuery).Find(&messages)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(messages)
+}
+
 // CREATES A NEW ENTRY IN THE DATABASE
 // REQUEST NEEDS TO PASS IN SENDER ID, RECEIVER ID, AND MESSAGE
 func createMessage(w http.ResponseWriter, r *http.Request) {
@@ -79,12 +94,47 @@ func createMessage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
+	// VALIDATE THE INFORMATION
+	// CHECK IF THE ID'S ARE NUMERIC
+	_, err = strconv.Atoi(message.Sender_ID)
+
+	if err != nil {
+		http.Error(w, "Invalid Sender ID (NOT NUMERIC): "+message.Sender_ID+" is not a valid ID.", http.StatusBadRequest)
+		return
+	}
+
+	_, err = strconv.Atoi(message.Receiver_ID)
+
+	if err != nil {
+		http.Error(w, "Invalid Receiver ID (NOT NUMERIC): "+message.Receiver_ID+" is not a valid ID.", http.StatusBadRequest)
+		return
+	}
+
+	// CHECK IF THE ID LENGTH IS FOUR
+	if len(message.Sender_ID) != 4 {
+		http.Error(w, "Invalid Sender ID (NOT FOUR DIGITS): "+message.Sender_ID+" is not a valid ID.", http.StatusBadRequest)
+		return
+	}
+
+	if len(message.Sender_ID) != 4 {
+		http.Error(w, "Invalid Receiver ID (NOT FOUR DIGITS): "+message.Receiver_ID+" is not a valid ID.", http.StatusBadRequest)
+		return
+	}
+
+	if len(message.Message) == 0 {
+		http.Error(w, "Invalid Message: Messages cannot be empty.", http.StatusBadRequest)
+		return
+	}
+
+	//IF IT PASSES THE CHECKS, THEN IT CREATES
 	result := db.Create(&message)
+
 	if result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
 	}
-	json.NewEncoder(w).Encode(message)
+	json.NewEncoder(w).Encode("Message created successfully.")
 }
 
 // UPDATES ONLY THE MESSAGE FIELD IN THE ENTRY
@@ -107,7 +157,7 @@ func editMessage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Message not found.", http.StatusNotFound)
 		return
 	}
-	json.NewEncoder(w).Encode(message)
+	json.NewEncoder(w).Encode("Message edited successfully.")
 }
 
 // SOFT DELETES A MESSAGE
@@ -136,7 +186,23 @@ func deleteMessage(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Message deleted successfully.")
 }
 
+func deleteTable(w http.ResponseWriter, r *http.Request) {
+	//CLEARS THE ENTIRE DATABASE - THIS WILL ONLY EVER BE USED FOR TESTING PURPOSES
+	//THIS RESETS THE GORM ID BACK TO 1
+	err = db.Exec("TRUNCATE TABLE user_messages").Error
+	if err != nil {
+    	panic(err)
+	}
+	json.NewEncoder(w).Encode("Table was deleted.")
+}
+
 func main() {
+
+	//CHECK IF THERE WERE ERRORS WITH CONNECTING
+	if err != nil {
+		panic("Error: Failed to connect to database.")
+	}
+
 	// INIT ROUTER
 	r := mux.NewRouter()
 
@@ -150,9 +216,11 @@ func main() {
 	// TEXT ROUTE HANDLERS / ENDPOINTS
 	r.HandleFunc("/api/messages/{id_1}/{id_2}", getConversation).Methods("GET")
 	r.HandleFunc("/api/messages", getAllMessages).Methods("GET")
+	r.HandleFunc("/api/messages/{search}", searchMessage).Methods("GET")
 	r.HandleFunc("/api/messages", createMessage).Methods("POST")
 	r.HandleFunc("/api/messages/{id_1}/{id_2}", editMessage).Methods("PUT")
 	r.HandleFunc("/api/messages/{id_1}/{id_2}", deleteMessage).Methods("DELETE")
+	r.HandleFunc("/api/messages/deleteTable", deleteTable).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
