1d242f7899c210b3aaa0e20c5eea44482561eb07
kevinc3n <92806132+kevinc3n@users.noreply.github.com>
2023-02-20 20:32:05 -0500

    Changed REST functions to be based on sender/receiver ID

--- GatorChat_Rest_API.go
+++ GatorChat_Rest_API.go
@@ -1,6 +1,6 @@
 package main
 
-//API IS INTERFACED USING POSTMAN
+//API IS INTERFACED/TESTED USING POSTMAN
 
 import (
 	"encoding/json"
@@ -30,17 +30,31 @@ type UserMessage struct {
 	Receiver_ID string `json:"receiver_id"`
 }
 
-// GETS A MESSAGE BASED ON THE GORM ID
-func getMessage(w http.ResponseWriter, r *http.Request) {
+// // GETS A MESSAGE BASED ON THE GORM ID
+// func getMessage(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r)
+// 	var message UserMessage
+// 	result := db.First(&message, params["id"])
+// 	if result.Error != nil {
+// 		http.Error(w, "Message not found.", http.StatusNotFound)
+// 		return
+// 	}
+// 	json.NewEncoder(w).Encode(message)
+// }
+
+// RETRIEVES ALL MESSAGES BETWEEN TWO PEOPLE
+// REQUEST NEEDS TO PASS IN SENDER ID AND RECEIVER ID
+func getConversation(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	var message UserMessage
-	result := db.First(&message, params["id"])
+	var messages []UserMessage
+	result := db.Where("(sender_id = ? OR receiver_id = ?) AND (sender_id = ? OR receiver_id = ?)", params["id_1"], params["id_1"], params["id_2"], params["id_2"]).Find(&messages)
 	if result.Error != nil {
-		http.Error(w, "Message not found.", http.StatusNotFound)
+		http.Error(w, "Messages not found.", http.StatusNotFound)
 		return
 	}
-	json.NewEncoder(w).Encode(message)
+	json.NewEncoder(w).Encode(messages)
 }
 
 // GETS ALL MESSAGES IN DATABASE
@@ -56,6 +70,7 @@ func getAllMessages(w http.ResponseWriter, r *http.Request) {
 }
 
 // CREATES A NEW ENTRY IN THE DATABASE
+// REQUEST NEEDS TO PASS IN SENDER ID, RECEIVER ID, AND MESSAGE
 func createMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var message UserMessage
@@ -73,6 +88,7 @@ func createMessage(w http.ResponseWriter, r *http.Request) {
 }
 
 // UPDATES ONLY THE MESSAGE FIELD IN THE ENTRY
+// REQUEST NEEDS TO PASS IN SENDER ID, RECEIVER ID, AND MESSAGE
 func editMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -82,7 +98,7 @@ func editMessage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	result := db.Model(&UserMessage{}).Where("id = ?", params["id"]).Update("Message", message.Message)
+	result := db.Model(&UserMessage{}).Where("sender_id = ? AND receiver_id = ?", params["id_1"], params["id_2"]).Update("Message", message.Message)
 	if result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
@@ -107,12 +123,12 @@ func editMessage(w http.ResponseWriter, r *http.Request) {
 //	json.NewEncoder(w).Encode("Message deleted successfully.")
 //}
 
-// HARD DELETES A MESSAGE (the ID can be reused after this)
+// HARD DELETES A MESSAGE (ID CAN BE REUSED AFTER THIS)
 func deleteMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	var userMessage UserMessage
-	result := db.Where("id = ?", params["id"]).Unscoped().Delete(&userMessage)
+	result := db.Where("sender_id = ? AND receiver_id = ?", params["id_1"], params["id_2"]).Unscoped().Delete(&userMessage)
 	if result.RowsAffected == 0 {
 		http.NotFound(w, r)
 		return
@@ -132,11 +148,11 @@ func main() {
 	// }
 
 	// TEXT ROUTE HANDLERS / ENDPOINTS
-	r.HandleFunc("/api/messages/{id}", getMessage).Methods("GET")
+	r.HandleFunc("/api/messages/{id_1}/{id_2}", getConversation).Methods("GET")
 	r.HandleFunc("/api/messages", getAllMessages).Methods("GET")
 	r.HandleFunc("/api/messages", createMessage).Methods("POST")
-	r.HandleFunc("/api/messages/{id}", editMessage).Methods("PUT")
-	r.HandleFunc("/api/messages/{id}", deleteMessage).Methods("DELETE")
+	r.HandleFunc("/api/messages/{id_1}/{id_2}", editMessage).Methods("PUT")
+	r.HandleFunc("/api/messages/{id_1}/{id_2}", deleteMessage).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
