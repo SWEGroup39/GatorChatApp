52cfbab67cfddb2f2238875b69b5c3ef71a6113b
kevinc3n <92806132+kevinc3n@users.noreply.github.com>
2023-02-26 22:23:37 -0500

    Added unit tests file and completed a POST unit test

---
+++ GatorChat_Rest_API_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"gorm.io/gorm"
+)
+
+func TestCreateMessage(t *testing.T) {
+	// CREATE A NEW USERMESSAGE STRUCT THAT WILL BE USED TO TEST THE POST
+	message := UserMessage{
+		Sender_ID:   "1234",
+		Receiver_ID: "5678",
+		Message:     "Hello, world!",
+	}
+
+	// TURN THE STRUCT INTO A JSON
+	requestBody, err := json.Marshal(message)
+	if err != nil {
+		t.Fatalf("Failed to marshal message: %s", err)
+	}
+
+	// CALL THE POST AND PASS IN THE USERMESSAGE STRUCT
+	req, err := http.NewRequest("POST", "/messages", bytes.NewBuffer(requestBody))
+	if err != nil {
+		t.Fatalf("Failed to create request: %s", err)
+	}
+
+	// CREATES A RESPONSE RECORDER
+	rr := httptest.NewRecorder()
+
+	// TEST THE POST BY PASSING IN THE RESPONSE RECORDER AND THE STRUCT
+	createMessage(rr, req)
+
+	// VERIFY THAT THE CALL WAS SUCCESSFUL
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, rr.Code)
+	}
+
+	// TURN THE JSON BACK INTO A STRUCT
+	var responseStruct UserMessage
+	err = json.Unmarshal(rr.Body.Bytes(), &responseStruct)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal response body: %s", err)
+	}
+
+	// CREATE THE EXPECTED USERMESSAGE STRUCT THAT SHOULD MATCH WITH THE ACTUAL STRUCT
+	expectedResponse := UserMessage{
+		// NOTE: THE GORM.MODEL VALUES ARE HARDCODED TO JUST BE EQUAL TO THE VALUES IN RESPONSESTRUCT
+		// THIS WAS DONE BECAUSE IT IS NOT POSSIBLE TO HARDCODE THE TIME (TOO SPECIFIC)
+		// ALSO, THE ID IS ALWAYS ALTERING WHICH MAKES HARDCODING THE ACTUAL ID DIFFICULT
+		// THESE PARAMETERS ARE HANDLED BY GORM, SO IT CAN BE ASSUMED THAT THESE ARE ALWAYS VALID
+		Model: gorm.Model{
+			ID:        responseStruct.ID,
+			CreatedAt: responseStruct.CreatedAt,
+			UpdatedAt: responseStruct.UpdatedAt,
+			DeletedAt: responseStruct.DeletedAt,
+		},
+		Sender_ID:   "1234",
+		Receiver_ID: "5678",
+		Message:     "Hello, world!",
+	}
+
+	// CHECK IF THE EXPECTED RESPONSE IS EQUAL TO THE ACTUAL RESPONSE
+	if !reflect.DeepEqual(responseStruct, expectedResponse) {
+		t.Errorf("Expected the response body '%v', but got '%v'", expectedResponse, responseStruct)
+	}
+}
