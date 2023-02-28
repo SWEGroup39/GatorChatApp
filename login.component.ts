2c7bb5f9a5b7489742e41e49bc52b9cc732ff405
RiaChacko2 <92886100+RiaChacko2@users.noreply.github.com>
2023-02-27 21:10:52 -0500

    Update login.component.ts
    Added the updated code for the login.ts file

--- login.component.ts
+++ login.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public getJsonValue: any;
+  public postJsonValue: any;
+  
+  user= null;
 
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit():void{
+    this.getMethod();
+
+  }
+
+  // public getMethod(){
+  //   this.http.get('http://localhost:8080/api/users').subscribe((data) => {
+  //     console.log(data);
+  //     this.getJsonValue = data;
+  //   });
+    
+  // }
+
+  public getMethod(){
+    this.http.get('http://localhost:8080/api/users').subscribe((data) => {
+      console.log(data);
+      this.getJsonValue = data;
+    });
+    
+  }
+
+  public postMethod(){
+    
+    this.http.post('http://localhost:8080/api/users',{}).subscribe((data) => {
+      console.log(data);
+      this.postJsonValue = data;              
+
+    });
+    
+  }
+
+  
 }
