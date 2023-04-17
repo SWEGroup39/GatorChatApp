691ff69ff91ff1272279a7649ec6dc859cde6c50
RiaChacko2 <92886100+RiaChacko2@users.noreply.github.com>
2023-04-17 15:58:23 -0400

    Integrated Both Front end member code
    Integrated and updated the messages, chat-list, styles of login and signup pages

--- app/components/messages/messages.component.ts
+++ app/components/messages/messages.component.ts
@@ -1,12 +1,12 @@
 import { HttpClient, HttpHeaders, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Component, ElementRef, ViewChild, OnInit, ChangeDetectorRef } from '@angular/core';
-import { EMPTY, Observable, of, throwError, interval } from 'rxjs';
+import { EMPTY, Observable, of, throwError, interval, timer, BehaviorSubject } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 //import {interval} from "rxjs/internal/observable/interval";
-import { tap, catchError } from 'rxjs/operators';
+import { tap, catchError, delay, delayWhen} from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { UserService } from 'src/app/service/user.service';
+
 
 
 
@@ -22,12 +22,17 @@ export class MessagesComponent implements OnInit{
   // variables 
   showMenu: boolean = false;
   editMode: boolean = false;
+  showUndoButton: boolean = false;
+  isImageUploaded: boolean = false;
   currentMessage: any;
   editedMessage: string = "";
   chatInputMessage: string = "";
   searchInputMessage: string = "";
-  localID:string=''
-  currentUsername:string=''
+  image: string = "";
+  lastDeletedMessage: number = 0;
+  longPollingInterval = 1000; 
+
+
   // Messages List
   chatMessages: {
     userId: number,
@@ -35,6 +40,7 @@ export class MessagesComponent implements OnInit{
     message: string,
     created_at: number,
     messageId: number,
+    image: File,
   }[] =[]
 
   APIurl: string = `http://localhost:8080/api/messages`;
@@ -48,20 +54,30 @@ export class MessagesComponent implements OnInit{
     name: 'null',
     id: 'null',
   }
+  idLog:string=''
+  @ViewChild('chatList', { static: true }) chatList!: ElementRef;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private location: Location, private cd: ChangeDetectorRef,private user:UserService) { }
+  constructor(private http: HttpClient, private route: ActivatedRoute, private location: Location, private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
-    this.localID = sessionStorage.getItem('idLog')??''
+    this.idLog = sessionStorage.getItem('idLog')??''
     this.route.queryParams.subscribe(params => {
       // this.currentUser.id  = params['id1'] ?? '0000';
       this.user1.id = params['id2'] ?? '0000';
-      this.currentUser.id = JSON.stringify(sessionStorage.getItem('currentUserI'+this.localID)).replace(/['"]/g, '');
-      this.currentUsername = JSON.stringify(sessionStorage.getItem('currentUserU'+this.localID)).replace(/['"]/g, '');
-
-      // set up long polling with RxJS
-      interval(1000).pipe(
-        switchMap(() => this.getMessagesLong(this.currentUser.id, this.user1.id)),
+      this.currentUser.id = JSON.stringify(sessionStorage.getItem('currentUserI'+this.idLog)).replace(/['"]/g, '');
+      
+      const intervalSubject = new BehaviorSubject(this.longPollingInterval); // create BehaviorSubject with initial interval time
+  
+      intervalSubject.pipe(
+        delayWhen(() => this.showMenu ? EMPTY : timer(5000)), // pause long polling if showMenu is true
+        switchMap(intervalTime => interval(intervalTime)), // switch to new interval observable with updated interval time
+        switchMap(() => {
+          if (this.showMenu) {
+            return EMPTY; // don't make API call if showMenu is true
+          } else {
+            return this.getMessagesLong(this.currentUser.id, this.user1.id); // make API call if showMenu is false
+          }
+        }),
         map(result => result.chatMessages)
       ).subscribe(messages => {
         // update the chatMessages array with the new messages
@@ -74,19 +90,23 @@ export class MessagesComponent implements OnInit{
       });
     });
   }
-  signOutMessages(){
-    sessionStorage.setItem('userLoggedIn','false')
+
+  onImageSelect(event: any) {
+     this.image = event.target.files[0];
+     this.isImageUploaded = true;
+     console.log("Image uploaded: " + this.image);
+    // const reader = new FileReader();
+    // reader.readAsDataURL(file);
+    // reader.onload = () => {
+    //  this.image = reader.result?.toString().split(',')[1] ?? '';
     
+    // };
   }
 
-  @ViewChild('chatList', { static: true }) chatList!: ElementRef;
-
-  public getMessagesLong(Id1: string, Id2: string): Observable<{ chatMessages: { userId: number, recieverId: number,messageId: number, message: string, created_at: number, updated_at: number, deleted_at: number}[] }> {
+  public getMessagesLong(Id1: string, Id2: string): Observable<{ chatMessages: { userId: number, recieverId: number,messageId: number, message: string, created_at: number, updated_at: number, deleted_at: number, image: File}[] }> {
     const url = `${this.APIurl}/${Id1}/${Id2}/longPoll`;
-    return interval(1000).pipe(
-      switchMap(() => this.http.get<{ ID: number, CreatedAt: number, UpdatedAt: number, DeletedAt: number, message: string, SenderId: number, RecieverId: number, messageId: number }[]>(url)),
+    return this.http.get<{ ID: number, CreatedAt: number, UpdatedAt: number, DeletedAt: number, message: string, SenderId: number, RecieverId: number, messageId: number, image: File }[]>(url).pipe(
       map((response: any[]) => {
-       // console.log('Response:', response);
         const chatMessages = response.map(item => ({
           userId: item.sender_id,
           recieverId: item.RecieverId,
@@ -95,6 +115,7 @@ export class MessagesComponent implements OnInit{
           created_at: new Date(item.CreatedAt).getTime(),
           updated_at: new Date(item.UpdatedAt).getTime(),
           deleted_at: new Date(item.DeletedAt).getTime(),
+          image: item.image ? atob(item.image) : null,
           day: new Date(item.CreatedAt)
         }));
         return { chatMessages };
@@ -104,31 +125,6 @@ export class MessagesComponent implements OnInit{
       })
     );
   }
-
-  // public getMessagesStart(Id1: string, Id2: string ): Observable<{ chatMessages: { userId: number, recieverId: number,messageId: number, message: string, created_at: number, updated_at: number, deleted_at: number}[] }> {
-  //   const url = `${this.APIurl}/${Id1}/${Id2}`;
-  //   return this.http.get<{ ID: number, CreatedAt: number, UpdatedAt: number, DeletedAt: number, message: string, SenderId: number, RecieverId: number, messageId: number }[]>(url)
-  //     .pipe(
-  //       map((response: any[]) => {
-  //         console.log('Response:', response);
-  //          const chatMessages = response.map(item => ({
-  //           userId: item.sender_id,
-  //           recieverId: item.RecieverId,
-  //           messageId: item.ID,
-  //           message: item.message,
-  //           created_at: new Date(item.CreatedAt).getTime(),
-  //           updated_at: new Date(item.UpdatedAt).getTime(),
-  //           deleted_at: new Date(item.DeletedAt).getTime(),
-  //           day: new Date(item.CreatedAt)
-  //         }));
-  //         return { chatMessages };
-  //       }),
-  //       tap((response: any) => {
-  //         console.log('body: ' + response.body);
-  //       })
-  //     );
-  // }
-
   
   goBack() {
     this.location.back();
@@ -166,13 +162,15 @@ isSameDate(time1: number, time2: number) {
 
   closeToggle() {
     this.currentMessage = null;
+    this.showMenu = false;
+    this.editMode = false;
+    console.log('Toggle Menu clossed->long polling resumes')
   }
 
-
-  showToggleMenu(event: MouseEvent, item: any) {
-    event.preventDefault();
-    this.currentMessage = item;
-    
+  showToggleMenu(event: any, message: any) {
+    this.currentMessage = message;
+    this.showMenu = true;
+    console.log('Toggle Menu oppened ->long polling paused')
   }
 
   convertTimestampToDay(timestamp: number): Date {
@@ -181,35 +179,86 @@ isSameDate(time1: number, time2: number) {
     return date;
   }
 
-  editMessage() {
-    // event.stopPropagation(); 
-    // console.log("Editing message:", this.currentMessage); // prevent event from bubbling up
-    // this.currentMessage = message;
-    // this.editMode = true;
-    // this.editedMessage = message.message;
+  editMesssage() {
+    this.editedMessage = this.currentMessage.message;
+    this.editMode = true;
+    console.log("Trying to edit message")
   }
 
-
+  saveEditedMessage() {
+    const body = { message: this.editedMessage };
+    const url = `${this.APIurl}/${this.currentMessage.messageId}`;
+    this.http.put(url, body).subscribe(() => {
+      this.editMode = false;
+      this.currentMessage.message = this.editedMessage;
+    });
+    console.log("saved message");
+  }
 
   delete(id: number) {
     console.log("Deleting message:", this.currentMessage);
+    this.lastDeletedMessage = this.currentMessage;
     this.deleteMessage(id).subscribe(
       (result) => {
           this.getMessagesLong( this.currentUser.id , this.user1.id  ).subscribe((result) => {
             this.chatMessages = result.chatMessages;
-        
+            console.log("Deleted Succesfully");
               });
         }
-      
     )
+    this.lastDeletedMessage = id;
+    this.showUndoButton = true;
+    setTimeout(() => this.showUndoButton= false, 5000);
 
-    console.log(this.chatMessages);
     this.cd.detectChanges();
+  }
 
+  undo()
+  {
+    const url = `${this.APIurl}/undo/${this.currentUser.id}`;
+    console.log('Recovering Message: ' + this.lastDeletedMessage);
+     const body = { };
+    this.http.put(url, body).subscribe(() => {
+      console.log("Recovered Succesfully");
+    });
+    this.showMenu = false;
+    this.showUndoButton = false;
   }
 
 
-  send() {
+  send()
+  {
+    if(this.isImageUploaded)
+      this.sendImageMessage();
+    
+    else
+      this.sendOnlyMessage();
+
+  }
+
+  sendImageMessage()
+  {
+    // Create a new FormData object
+    let formData = new FormData();
+    formData.append('sender_id', this.currentUser.id);
+    formData.append('receiver_id', this.user1.id);
+    formData.append('message', this.chatInputMessage);
+    formData.append('image', this.image);
+
+    // Send the POST request
+    fetch(`${this.APIurl}/image`, {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+    this.isImageUploaded = false;
+    this.chatInputMessage = "";
+  }
+
+  sendOnlyMessage() {
     const newMessage = {
       message: this.chatInputMessage,
       userId: this.currentUser.id,
@@ -261,9 +310,6 @@ search(message: string) {
 
     });
   }
-
-
-  // API functions (Request)
 
   searchMessages(content: string): Observable<{ Messages: { messageId: number }[] }> {
     const url = `http://localhost:8080/api/messages/${this.currentUser.id}/${this.user1.id}/search`;
@@ -321,8 +367,6 @@ search(message: string) {
     );
   }
 
-
 }
-
 
 
